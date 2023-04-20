14e79f3474de33f3eb0dfb1b0b7451be19176dbd
John Hudson <jphudson@users.noreply.github.com>
2023-04-20 15:11:48 -0500

    initial commit

---
+++ connect.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def get_weather_data(city_name, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
