74d809182ffaee06cd934b61e6dd35efe4351f5b
John Hudson <jphudson@users.noreply.github.com>
2023-04-20 19:56:47 -0500

    learning forecast api

--- connect.py
+++ connect.py
@@ -2,7 +2,16 @@ import requests
 
 
 def get_weather_data(city_name, api_key):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def get_forecast_data(city_name, api_key):
+    url = f"https://api.openweathermap.org/data/2.5/forecast/?q={city_name}&appid={api_key}&units=imperial"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+import json
+
 from config import api_key
 from connect import *
 
-# Press the green button in the gutter to run the script.
+# jsons from openweathermap
 if __name__ == '__main__':
-    city_name = input("Enter city name: ")
+    #city_name = input("Enter city name: ")
+    city_name = "New Orleans"
 
     weather_data = get_weather_data(city_name, api_key)
+    forecast_data = get_forecast_data(city_name, api_key)
 
     if weather_data:
-        print(f"Current temperature: {weather_data['main']['temp']}°C")
+        print(f"Current temperature: {weather_data['main']['temp']}°F")
         print(f"Humidity: {weather_data['main']['humidity']}%")
         print(f"Wind speed: {weather_data['wind']['speed']} m/s")
+        print(f"Day: {weather_data['']}")
+
+        print("\n")
+        print(f"Weather Forecast for tomorrow: ")
+        print(f"Day: {forecast_data['list'][0]['dt_txt']}")
+        print(f"temp: {forecast_data['list'][0]['main']['temp']}°F")
     else:
         print("Unable to retrieve weather data.")
